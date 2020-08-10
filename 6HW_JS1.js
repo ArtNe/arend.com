300af2d87e6aed28ecf52529f3daff33e7323345
Artur Nechurenko <49311617+artur-nech@users.noreply.github.com>
2020-08-10 14:00:11 -0400

    HW

--- 6HW_JS1.js
+++ 6HW_JS1.js
@@ -1,9 +1,10 @@
-
-
-
-
+/*
+Just a simple sorting usage. Create a function that
+returns the elements of the input-array in a sorted manner.
+*/
 sortme = function( names )
     {
+    var names = ();
     names = int(input());
     console.log(names);
     names.sort();
